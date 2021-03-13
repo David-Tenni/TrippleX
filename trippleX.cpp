bed18eb260fbb0785b7a2e222e11a16d31c594f2
DivadTenni <davidctenni@gmail.com>
2021-03-14 10:26:32 +1100

    adding the hints

--- trippleX.cpp
+++ trippleX.cpp
@@ -5,18 +5,19 @@ int main()
     //prints starting messages
     std::cout << "Welcome to the World of Machines"<< std::endl;
     std::cout << "We need you to perform robo brain surgery on these robots"<< std::endl;
+    std::cout << "The code to save the robot has three numbers"<< std::endl;
 
 
     //decalres the variables 
     int a = 2;
-    int b = 1;
+    int b = 4;
     int c = 4;
     
     int sum = a + b + c;
     int product = a * b * c;
 
-    std::cout << sum << std::endl;   
-    std::cout << product;
+    std::cout << "The code's numbers add up to: "<< sum << std::endl;   
+    std::cout << "The code's numbers multiply to: " << product;
 
     return 0;
 }
