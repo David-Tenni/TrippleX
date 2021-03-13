d38fe90ba44d23685916e6bcee9602e1bae73e31
DivadTenni <davidctenni@gmail.com>
2021-03-13 11:31:12 +1100

    added linebreak

--- trippleX.cpp
+++ trippleX.cpp
@@ -3,6 +3,7 @@
 int main() 
 {    
     std::cout << "Welcome to the World of Machines";
+    std::cout << std::endl;
     std::cout << "We need you to perform robo brain surgery on these robots";
     return 0;
 }
