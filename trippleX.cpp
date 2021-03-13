017db4888c8077af8614733a9da05276d835ac10
DivadTenni <davidctenni@gmail.com>
2021-03-13 12:02:59 +1100

    optimisation

--- trippleX.cpp
+++ trippleX.cpp
@@ -2,19 +2,21 @@
 
 int main() 
 {    
-    std::cout << "Welcome to the World of Machines";
-    std::cout << std::endl;
+    //prints starting messages
+    std::cout << "Welcome to the World of Machines"<< std::endl;
     std::cout << "We need you to perform robo brain surgery on these robots"<< std::endl;
 
+
+    //decalres the variables 
     int a = 2;
     int b = 1;
     int c = 4;
     
     int sum = a + b + c;
-    int multsum = a * b * c;
+    int product = a * b * c;
 
     std::cout << sum << std::endl;   
-    std::cout << multsum;
+    std::cout << product;
 
     return 0;
 }
