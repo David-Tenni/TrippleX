2d7b131b94043ed05edca35b2d317ac4f7af1f15
DivadTenni <davidctenni@gmail.com>
2021-03-13 11:34:38 +1100

    adding variables

--- trippleX.cpp
+++ trippleX.cpp
@@ -5,5 +5,12 @@ int main()
     std::cout << "Welcome to the World of Machines";
     std::cout << std::endl;
     std::cout << "We need you to perform robo brain surgery on these robots";
+
+    int a = 2;
+    int b = 3;
+    
+    std::cout << std::endl;
+    std::cout << a;
+
     return 0;
 }
