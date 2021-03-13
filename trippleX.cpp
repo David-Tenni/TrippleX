002cd22427122b4b70c6aca8fa33728ff5cba7d4
DivadTenni <davidctenni@gmail.com>
2021-03-13 11:28:52 +1100

    creative story concept lel

--- trippleX.cpp
+++ trippleX.cpp
@@ -3,6 +3,6 @@
 int main() 
 {    
     std::cout << "Welcome to the World of Machines";
-    std::cout << "Welcome to the World of Machines";
+    std::cout << "We need you to perform robo brain surgery on these robots";
     return 0;
 }
