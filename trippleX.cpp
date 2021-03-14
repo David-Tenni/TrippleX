6d52991532096148faca7ffb362e1a4f7bb80381
DivadTenni <davidctenni@gmail.com>
2021-03-14 17:20:41 +1100

    fixed magic bug that was dumb

--- trippleX.cpp
+++ trippleX.cpp
@@ -8,25 +8,28 @@ int main()
     std::cout << "The code to save the robot has three numbers"<< std::endl;
 
 
-    //decalres the code 
-    int CodeA = 2, int CodeB = 4, int CodeC = 4;
-    //initalises player guess variables
-    int GuessA, int GuessB, int GuessC;
+    //decalres the variables 
+    int CodeA = 2;
+    int CodeB = 4;
+    int CodeC = 4;
+    
 
     int CodeSum = CodeA + CodeB + CodeC;
     int CodeProduct = CodeA * CodeB * CodeC;
 
+    std::cout << "There is three numbers in the code " << std::endl;   
     std::cout << "The code's numbers add up to: "<< CodeSum << std::endl;   
     std::cout << "The code's numbers multiply to: " << CodeProduct<< std::endl;
     
-    std::cout << "Please input the first number you think is in the code"<< std::endl;
-    std::cin >> GuessA;    
-    
-    std::cout << "Please input the second number you think is in the code"<< std::endl;
-    std::cin >> GuessB;
-     
-    std::cout << "Please input the third number you think is in the code"<< std::endl;
-    std::cin >> GuessC;
+    std::cout << "Please input the the numbers you believe makeup the code"<< std::endl;
+   // int GuessA, int GuessB, int GuessC;
+    int GuessA;
+    int GuessB;
+    int GuessC;
+    std::cin >> GuessA;
+    std::cin >> GuessB;    
+    std::cin >> GuessC;    
+
 
 
     std::cout << "your attempt is: " << GuessA << GuessB << GuessC;
